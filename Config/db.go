c9421a7b790298e750f4d8b28e6499447e2955a6
Julker Nien Akib <35792784+jakib01@users.noreply.github.com>
2023-11-08 19:08:52 +0600

    Revert ":pencil2: Add:-> domain driven architecture introduced"
    This reverts commit 242cbe40dc9f9e05fe08ad11ad8b91a6b721c274.

--- Config/db.go
+++ Config/db.go
@@ -12,56 +12,56 @@ import (
 var DB *gorm.DB
 
 type DBConfig struct {
-	Host     string
-	Port     int
-	User     string
-	DBName   string
-	Password string
+    Host     string
+    Port     int
+    User     string
+    DBName   string
+    Password string
 }
 
 func BuildDBConfig() *DBConfig {
-	dbConfig := DBConfig{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     portConv(os.Getenv("DB_PORT")),
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DBName:   os.Getenv("DB_NAME"),
-	}
-	return &dbConfig
+    dbConfig := DBConfig{
+        Host:     os.Getenv("DB_HOST"),
+        Port:     portConv(os.Getenv("DB_PORT")),
+        User:     os.Getenv("DB_USER"),
+        Password: os.Getenv("DB_PASSWORD"),
+        DBName:   os.Getenv("DB_NAME"),
+    }
+    return &dbConfig
 }
 
 // convert string to int
 func portConv(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err)
-	}
-	return i
+    i, err := strconv.Atoi(s)
+    if err != nil {
+        panic(err)
+    }
+    return i
 }
 
 func DbURL(dbConfig *DBConfig) string {
-	return fmt.Sprintf(
-		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
-		dbConfig.User,
-		dbConfig.Password,
-		dbConfig.Host,
-		dbConfig.Port,
-		dbConfig.DBName,
-	)
+    return fmt.Sprintf(
+        "%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+        dbConfig.User,
+        dbConfig.Password,
+        dbConfig.Host,
+        dbConfig.Port,
+        dbConfig.DBName,
+    )
 }
 
 func DatabaseInit() {
-	dbConfig := BuildDBConfig()
-	dbURL := DbURL(dbConfig)
+    dbConfig := BuildDBConfig()
+    dbURL := DbURL(dbConfig)
 
-	var err error
-	DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{})
-
-	if err != nil {
-		panic("Failed to connect to database")
-	}
+    var err error
+    DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{})
+    
+    if err != nil {
+        panic("Failed to connect to database")
+    }
 }
 
 func GetDB() *gorm.DB {
-	return DB
+    return DB
 }
--- Routes/routes.go
+++ Routes/routes.go
@@ -8,14 +8,8 @@ import (
 
 // SetupRoutes setup for routing
 func SetupRoutes(e *echo.Echo) {
-	productHandler := Handler.NewProductHandler()
-
 	e.GET("/course", Handler.GetCourse)
 	e.POST("/course", Handler.CreateCourse)
 	e.PUT("/course/:id", Handler.UpdateCourse)
 	e.DELETE("/course/:id", Handler.DeleteCourse)
-
-	// Product route
-	e.GET("/product", productHandler.GetProduct)
-	e.POST("/product-create", productHandler.CreateProduct)
 }
